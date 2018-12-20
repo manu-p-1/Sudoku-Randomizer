71cc0dcf12d1841a12a6e3fe4e21bc2e119ad3fa
Manu <28782438+manu-p-1@users.noreply.github.com>
2018-12-19 20:39:04 -0500

    Added some javadoc to Tester.java

--- src/projects/randomizer/Tester.java
+++ src/projects/randomizer/Tester.java
@@ -2,21 +2,24 @@ package projects.randomizer;
 
 import java.util.Arrays;
 
+/**
+* A simple class to demonstrate the SudokuRandomizer class
+*/
 public class Tester {
-	
+
 	public static void main(String[] args) {
-		
-		Integer[][] premade = { 
+
+		Integer[][] premade = {
 				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
 				{ 4, 5, 6, 7, 8, 9, 1, 2, 3 },
-				{ 7, 8, 9, 1, 2, 3, 4, 5, 6 }, 
+				{ 7, 8, 9, 1, 2, 3, 4, 5, 6 },
 				{ 2, 3, 4, 5, 6, 7, 8, 9, 1 },
-				{ 5, 6, 7, 8, 9, 1, 2, 3, 4 }, 
+				{ 5, 6, 7, 8, 9, 1, 2, 3, 4 },
 				{ 8, 9, 1, 2, 3, 4, 5, 6, 7 },
-				{ 3, 4, 5, 6, 7, 8, 9, 1, 2 }, 
+				{ 3, 4, 5, 6, 7, 8, 9, 1, 2 },
 				{ 6, 7, 8, 9, 1, 2, 3, 4, 5 },
-				{ 9, 1, 2, 3, 4, 5, 6, 7, 8 } };	
-		
+				{ 9, 1, 2, 3, 4, 5, 6, 7, 8 } };
+
 		print(premade);
 		SudokuRandomizer.swapRowInGroup(premade);
 		System.out.println();
@@ -27,8 +30,8 @@ public class Tester {
 		System.out.println();
 		print(premade);
 	}
-	
-	
+
+
 	static void print(Integer[][] array) {
 		System.out.println(Arrays.deepToString(array)
 				.replace("], ", "]\n")
